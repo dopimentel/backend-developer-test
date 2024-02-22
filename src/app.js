259e980edabaa9656e9d5024010f8884ee8f4620
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-22 19:58:11 -0300

    GET /companies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const pool = require('./models/db');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API Job Posting');
-  });
+app.use(express.json());
+
+// Rota de exemplo
+app.get('/', async (req, res) => {
+ try {
+   const result = await pool.query('SELECT $1::text as message', ['Hello World']);
+   res.json({ message: result.rows[0].message });
+ } catch (error) {
+   console.error('Erro na consulta ao banco de dados:', error);
+   res.status(500).json({ error: 'Erro interno do servidor' });
+ }
+});
+
+app.get('/companies', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM companies');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erro na consulta ao banco de dados:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
 
 module.exports = app;

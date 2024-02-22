f759e250c2ab1c658cd8bf053c632d0199570e1f
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-22 18:22:09 -0300

    environment variables configs

---
+++ config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//     database: {
+//       host: 'localhost',
+//       user: 'user',
+//       password: 'password',
+//       database: 'jobpostingdb',
+//       port: 5432,
+//     },
+//     aws: {
+//       accessKeyId: 'sua-chave-de-acesso-aws',
+//       secretAccessKey: 'seu-segredo-de-acesso-aws',
+//       region: 'sua-regiao-aws',
+//       s3Bucket: 'seu-bucket-s3',
+//     },
+//   };
--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'user',
-  host: 'localhost',
-  database: 'jobpostingdb',
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER || 'user',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_DATABASE || 'jobpostingdb',
+  password: process.env.DB_PASSWORD || 'password',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;

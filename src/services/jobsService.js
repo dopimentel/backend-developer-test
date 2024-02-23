0d4e6de1843976629d86f1b51b57437096667767
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-23 13:38:36 -0300

    remove tracking .env

--- src/services/jobsService.js
+++ src/services/jobsService.js
@@ -1,7 +1,9 @@
 const jobsModel = require('../models/jobsModel');
+const { sendMessageToSQS } = require('./sqsService');
 
 const create = async (job) => {
     const newJob = await jobsModel.create(job);
+    await sendMessageToSQS(newJob);
     return { status: 'SUCCESSFUL', message: newJob };
 };
 
